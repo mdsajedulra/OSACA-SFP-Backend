8cda5998d7e4a7e8ad23bd4ce3d1c762ed66354a
Md Sajedul Islam <65823258+mdsajedulra@users.noreply.github.com>
2026-03-20 10:43:54 +0600

    distribution food

--- src/app/modules/foodDistributions/distribution.controller.ts
+++ src/app/modules/foodDistributions/distribution.controller.ts
@@ -3,6 +3,7 @@ import catchAsync from "../../utils/catchAsync";
 import sendResponse from "../../utils/sendResponse";
 import { distributionServices } from "./distribution.service";
 import { ObjectId } from "mongodb";
+import { get } from "mongoose";
 
 const createDistribution = catchAsync(async (req, res)=>{
     const distribution = await distributionServices.createDistribution(req.body);
@@ -14,6 +15,20 @@ const createDistribution = catchAsync(async (req, res)=>{
 
     })
 })
+
+// get all distribution
+
+const getAllDistributions = catchAsync(async (req, res)=>{
+    const distributions = await distributionServices.getAllDistributions();
+    sendResponse(res, {
+        success: true,
+        statusCode: StatusCodes.OK,
+        message: "Food distributions retrieved successfully",
+        data: distributions,
+    });
+});
+
+
 
 // get distribution by id 
 
@@ -27,7 +42,33 @@ const getDistributionById = catchAsync(async (req, res)=>{
     });
 });
 
+// update distribution by id
+
+const updateDistributionById = catchAsync(async (req, res)=>{
+    const distribution = await distributionServices.updateDistributionById(req.params.id as unknown as ObjectId, req.body);
+    sendResponse(res, {
+        success: true,
+        statusCode: StatusCodes.OK,
+        message: "Distribution updated successfully",
+        data: distribution,
+    });
+})
+
+// delete distribution by id
+
+const deleteDistributionById = catchAsync(async (req, res)=>{
+    await distributionServices.deleteDistributionById(req.params.id as unknown as ObjectId);
+    sendResponse(res, {
+        success: true,
+        statusCode: StatusCodes.OK,
+        message: "Distribution deleted successfully",
+    });
+})
+
 export const distributionController = {
     createDistribution,
     getDistributionById,
+    getAllDistributions,
+    updateDistributionById,
+    deleteDistributionById,
 }
--- src/app/modules/foodDistributions/distribution.routes.ts
+++ src/app/modules/foodDistributions/distribution.routes.ts
@@ -5,7 +5,9 @@ const distributionRouter = Router();
 
 
 distributionRouter.post("/",  distributionController.createDistribution)
+distributionRouter.get("/", distributionController.getAllDistributions)
 distributionRouter.get("/:id", distributionController.getDistributionById)
-
+distributionRouter.patch("/:id", distributionController.updateDistributionById)
+distributionRouter.delete("/:id", distributionController.deleteDistributionById)
 
 export default distributionRouter;
